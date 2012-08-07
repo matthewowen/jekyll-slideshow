20fe40248c1b57ac8a3048e13a40a11f84dfe493
Matthew Owen <matthew.owen95@gmail.com>
2012-08-07 20:52:45 +0100

    updates, tweaks, fixes

--- _plugins/jekyll_slideshow.rb
+++ _plugins/jekyll_slideshow.rb
@@ -14,14 +14,19 @@ module Jekyll
       else 
         config = Hash["width", 100, "height", 100]
       end
+      to_thumb = Array.new
       site.static_files.each do |file|
-        if File.extname(file.path).downcase == ('.jpg' || '.png') && file.path.index("-thumb") == nil
-            img = Magick::Image::read(file.path).first
-            thumb = img.resize_to_fill(config['width'], config['height'])
-            path = file.path.sub(File.extname(file.path), '-thumb' << File.extname(file.path))
-            thumb.write path
-            site.static_files << StaticFile.new(thumb, site.source, File.dirname(file.path).sub(site.source, ''), File.basename(file.path).sub('.JPG', '-thumb.JPG'))
+        if (File.extname(file.path).downcase == ('.jpg' || '.png')) && (!File.basename(file.path).include? "-thumb")
+            to_thumb.push(file)
         end
+      end
+      to_thumb.each do |file|
+          img = Magick::Image::read(file.path).first
+          thumb = img.resize_to_fill(config['width'], config['height'])
+          path = file.path.sub(File.extname(file.path), '-thumb' << File.extname(file.path))
+          thumb.write path
+          site.static_files << StaticFile.new(thumb, site.source, File.dirname(file.path).sub(site.source, ''), File.basename(file.path).sub('.JPG', '-thumb.JPG'))
+          puts file
       end
     end
   end
